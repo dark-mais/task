42e8ffff38a83793b8e78a723887e6dca51c4b8c
dark-mais <dark1mais@gmail.com>
2024-07-04 15:55:11 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            // Попытка выполнить код, который может выбросить исключение
+            int result = divide(10, 0);
+            System.out.println("Результат: " + result);
+        } catch (ArithmeticException e) {
+            // Обработка ArithmeticException, которое может быть выброшено при делении на ноль
+            System.out.println("Ошибка: Невозможно делить на ноль!");
+        } catch (Exception e) {
+            // Обработка всех остальных исключений
+            System.out.println("Произошла ошибка: " + e.getMessage());
+        } finally {
+            // Этот блок выполняется в любом случае, независимо от того, было ли исключение или нет
+            System.out.println("Блок finally выполнен.");
         }
+    }
+
+    public static int divide(int a, int b) throws ArithmeticException {
+        return a / b;
     }
 }
